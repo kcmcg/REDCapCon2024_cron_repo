10ca430dce2e9b3c133b0e53e863ec6eae93c219
Kyle McGuffin <kyle.mcguffin@vumc.org>
2024-09-11 13:37:22 -0400

    Initial push

---
+++ testDataPull.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace VUMC\testDataPull;
+
+use ExternalModules\AbstractExternalModule;
+
+class testDataPull extends AbstractExternalModule
+{
+	function run_cron($params){
+		// REDCap::getData here
+	}
+}
